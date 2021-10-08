331295053955b53e8d5e1419453cda2a32ed9e83
Philip R. McDavid <frameoflightdesigner@gmail.com>
2021-10-08 12:40:48 -0400

    improvement commitment

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios";
+import {BASE_URL} from "./Constants/Index.js";
+import Character from "./components/Character";
+import styled from 'styled-components';
+import images from './images/sw-bg.jpg';
+
+const StyledBackground = styled.div`
+  background-image: url(${images});
+  background-size: cover;
+  padding: 50px;
+`
+
+const StyledHeader = styled.h1`
+  font-size: 4em;
+`
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+    // Try to think through what state you'll need for this app before starting. Then build out
+    // the state properties here.
+    const [characters, setCharacters] = useState([])
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+    // Fetch characters from the API in an effect hook. Remember, anytime you have a
+    // side effect in a component, you want to think about which state and/or props it should
+    // sync up with, if any.
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
+    useEffect(() => {
+        axios.get(`${BASE_URL}`)
+            .then(res => {
+                //console.log(res.data);
+                setCharacters(res.data);
+            }).catch(err => console.log(err))
+    }, [])
+    //console.log(characters)
+    return (
+        <StyledBackground className="App">
+            <StyledHeader className="Header">Star Wars</StyledHeader>
+            <Character className="Characters" characters={characters}/>
+        </StyledBackground>
+    );
 }
 
 export default App;
